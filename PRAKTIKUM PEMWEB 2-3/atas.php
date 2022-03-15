f9566b421cc8f16b77442e70982aed9733072c37
Bayuuptr <92218708+Bayuuptr@users.noreply.github.com>
2022-03-15 21:00:58 +0700

    Add files via upload

---
+++ PRAKTIKUM PEMWEB 2-3/atas.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SAS-STTNF</title>
+</head>
+<body>
+    <header role="banner">
+        <h2 class="text-center">Student Activity Score - STTNF</h2>
+        <hr/>
+    </header>
